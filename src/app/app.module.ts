f2d82f8065e7124ed6292764590721cd44b401e8
Gabriel Bastos <gbastosbarbosa@gmail.com>
2022-01-17 13:42:04 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { SobreComponent } from './components/template/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HeaderComponent,
+    SobreComponent,
   ],
   imports: [
     BrowserModule,
