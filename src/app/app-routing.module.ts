ec73875a98ff622bfca02587ce47ade692424871
Gabriel Bastos <gbastosbarbosa@gmail.com>
2022-01-17 19:43:37 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './components/template/contatos/contatos.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { ProjetosComponent } from './components/template/projetos/projetos.component';
+import { SobreComponent } from './components/template/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HeaderComponent },
+  { path: "sobre", component: SobreComponent },
+  { path: "projetos", component: ProjetosComponent },
+  { path: "contatos", component: ContatosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
